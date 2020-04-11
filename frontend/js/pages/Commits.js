79b7d2d1d134fd8560397b3d22ea365f565cec87
Hazin <bernardo.hazin@mesainc.com.br>
2020-04-10 23:39:11 -0300

    Setting pagination on commits page

--- frontend/js/pages/Commits.js
+++ frontend/js/pages/Commits.js
@@ -1,13 +1,35 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
 import Card from '../components/commits/Card';
+import Pagination from '../components/commits/Pagination';
+import { INITIAL_PAGE, INITIAL_PER_PAGE } from '../constants';
+import { setCommits, setTotal } from '../state/actions';
+import { search } from '../utils';
 
-const Commits = ({ commits }) => {
-  const history = useHistory();
-  if (!commits.length) history.push('/');
+const Commits = ({ commits, total, dispatch }) => {
+  const fetch = (page, per_page) => {
+    const params = { per_page, page };
+    search({
+      params,
+      onSuccess: (res) => {
+        dispatch(setCommits(res.data.data));
+        dispatch(setTotal(res.data.total));
+      },
+      onError: (err) => {
+        if (err.response.status === 403) {
+          window.location.replace('/');
+        }
+      },
+    });
+  };
+
+  useEffect(() => {
+    fetch(INITIAL_PAGE, INITIAL_PER_PAGE);
+  }, [window.location]);
+
   return (
     <div className="commits-container column ac">
       <ul className="list slideUp">
@@ -17,14 +39,18 @@ const Commits = ({ commits }) => {
           </li>
         ))}
       </ul>
+      <Pagination className="slideUp" fetch={fetch} total={total} />
     </div>
   );
 };
 
 Commits.propTypes = {
   commits: PropTypes.array.isRequired,
+  total: PropTypes.number.isRequired,
+  dispatch: PropTypes.func,
 };
 
 export default connect((state) => ({
-  commits: state.commits,
+  commits: state.commitsReducer.commits,
+  total: state.commitsReducer.total,
 }))(Commits);
