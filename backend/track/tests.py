e9ff7f1dbd4e90de42b4cbf5769a74dc155b4d74
Hazin <bernardo.hazin@mesainc.com.br>
2020-04-15 15:37:05 -0300

    Adding services and models tests

--- backend/track/tests.py
+++ backend/track/tests.py
@@ -1,12 +1,14 @@
 from django.contrib.messages.storage.fallback import FallbackStorage
 from django.contrib.sessions.middleware import SessionMiddleware
-from django.test import RequestFactory, TestCase
+from django.test import RequestFactory, TestCase, SimpleTestCase
 from django.urls import reverse
 from django.test import TestCase
+from django.db import IntegrityError
 from .views import index, me, search, search_filtered_commits
-from .models import Commit
+from .models import Commit, Hooks
 import json
 import datetime
+from .services import get_last_month, serialize_commit_response
 
 
 class IndexViewTests(TestCase):
@@ -168,3 +170,106 @@ class FilteredSearchViewTests(TestCase):
         data = json.loads(response.content.decode('utf-8'))
         self.assertEqual(data['total'], 2)
         self.assertEqual(len(data['data']), 2)
+
+
+class CommitTestCase(TestCase):
+    # pylint: disable = no-member
+    def test_should_fail_if_sha_is_not_unique(self):
+        sha = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+        date = datetime.datetime.now()
+        Commit.objects.create(sha=sha, date=date)
+        self.assertRaises(
+            IntegrityError, Commit.objects.create, sha=sha, date=date
+        )
+
+    def test_should_fail_if_date_is_null(self):
+        sha = "d92431e5127244c07f9264467d9ac3fa3ef5fbe9"
+        self.assertRaises(
+            IntegrityError, Commit.objects.create, sha=sha
+        )
+
+    def test_should_fail_if_committer_exceed_max_length(self):
+        committer = "a" * 256
+        self.assertRaises(
+            IntegrityError, Commit.objects.create, committer=committer
+        )
+
+    def test_should_fail_if_message_exceed_max_length(self):
+        message = "a" * 256
+        self.assertRaises(
+            IntegrityError, Commit.objects.create, message=message
+        )
+
+    def test_should_fail_if_url_exceed_max_length(self):
+        url = "a" * 256
+        self.assertRaises(
+            IntegrityError, Commit.objects.create, url=url
+        )
+
+    def test_should_fail_if_user_exceed_max_length(self):
+        user = "a" * 256
+        self.assertRaises(
+            IntegrityError, Commit.objects.create, user=user
+        )
+
+    def test_should_fail_if_project_exceed_max_length(self):
+        project = "a" * 256
+        self.assertRaises(
+            IntegrityError, Commit.objects.create, project=project
+        )
+
+
+class HookTestCase(TestCase):
+    # pylint: disable = no-member
+    def test_should_fail_if_project_is_not_unique(self):
+        project = "project"
+        Hooks.objects.create(id=1, project=project)
+        self.assertRaises(
+            IntegrityError, Hooks.objects.create, id=2, project=project
+        )
+
+
+class GetLastMonthServiceTests(SimpleTestCase):
+    def test_should_return_last_month_day_one(self):
+        response = get_last_month()
+        today = datetime.datetime.now()
+        first = today.replace(day=1)
+        lastMonth = first - datetime.timedelta(days=1)
+        date = lastMonth.replace(
+            day=1,
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0
+        ).isoformat()
+        self.assertEqual(date, response)
+
+
+class SerializeCommitResponseServiceTests(SimpleTestCase):
+    def setUp(self):
+        self.commit = {
+            "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+            "commit": {
+                "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+                "author": {
+                    "name": "Monalisa Octocat",
+                    "date": "2011-04-14T16:00:49Z"
+                },
+                "committer": {
+                    "name": "Monalisa Octocat",
+                    "email": "support@github.com",
+                    "date": "2011-04-14T16:00:49Z"
+                },
+                "message": "Fix all the bugs",
+            },
+        }
+
+    def test_should_return_last_month_day_one(self):
+        response = serialize_commit_response(self.commit)
+        self.assertDictEqual(response, {
+            'sha': self.commit['sha'],
+            'committer': self.commit['commit']['author']['name'],
+            'date': self.commit['commit']['author']['date'],
+            'message': self.commit['commit']['message'],
+            'url': self.commit['commit']['url'],
+        })
--- backend/track/views.py
+++ backend/track/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponseForbidden
 from django.http import HttpResponseBadRequest
 from django.http import Http404
 from django.http import JsonResponse
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 from os import environ
@@ -16,32 +15,10 @@ import json
 import datetime
 import hmac
 import hashlib
+from .services import get_commits, get_last_month, set_webhook
 
 
 import requests
-
-
-def get_last_month():
-    today = datetime.datetime.now()
-    first = today.replace(day=1)
-    lastMonth = first - datetime.timedelta(days=1)
-    return lastMonth.replace(
-        day=1,
-        hour=0,
-        minute=0,
-        second=0,
-        microsecond=0
-    ).isoformat()
-
-
-def serialize_commit_response(commit):
-    return {
-        'sha': commit['sha'],
-        'committer': commit['commit']['author']['name'],
-        'date': commit['commit']['author']['date'],
-        'message': commit['commit']['message'],
-        'url': commit['commit']['url'],
-    }
 
 
 def index(request):
@@ -61,26 +38,6 @@ def me(request):
         'avatar_url': request.session.__getitem__('avatar_url'),
         'location': request.session.__getitem__('location'),
     })
-
-
-def get_commits(access_token, login, project):
-    # pylint: disable=no-member
-    headers = {}
-    if access_token is not None:
-        headers = {'Authorization': 'token ' + access_token}
-    path = 'https://api.github.com/repos/' + login + '/' + project + '/commits'
-    r = requests.get(
-        path, params={'since': get_last_month()}, headers=headers)
-
-    if 'message' in r.json():
-        return r.json()
-
-    for el in list(map(serialize_commit_response, r.json())):
-        el['project'] = project
-        el['user'] = login
-        Commit.objects.update_or_create(sha=el['sha'], defaults=el)
-
-    return False
 
 
 def search_filtered_commits(request, project):
@@ -130,38 +87,6 @@ def search(request):
         return JsonResponse(
             {'data': list(page_obj), 'total': total}, safe=False)
     return Http404()
-
-
-def set_webhook(project, owner, access_token):
-    # pylint: disable = no-member
-    projectExist = None
-    try:
-        projectExist = Hooks.objects.get(project=project)
-    except ObjectDoesNotExist as error:
-        pass
-
-    if not projectExist:
-        path = 'https://api.github.com/repos/' + owner + '/' + project + '/hooks'
-        headers = {'Authorization': 'token ' + access_token}
-        data = {
-            "name": "web",
-            "active": True,
-            "events": [
-                "push"
-            ],
-            "config": {
-                "url": environ.get('BACKEND_BASE_URL') + '/track/webhook/',
-                "content_type": "json",
-                "secret": environ.get('WEBHOOK_SECRET'),
-                "insecure_ssl": environ.get('INSECURE_SSL')
-            }
-        }
-        print(path)
-        r = requests.post(path, headers=headers, json=data)
-        json = r.json()
-        if 'id' in json:
-            Hooks.objects.create(id=json['id'], project=project)
-            print('Created', json)
 
 
 @csrf_exempt
